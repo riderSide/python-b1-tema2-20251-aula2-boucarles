0b107f02da40abe150e68f9b678ce3b543e42618
rideSide <cbou@zat.net>
2025-12-08 23:36:30 +0100

    entrega ej2d2

--- 2d/ej2d2.py
+++ 2d/ej2d2.py
@@ -37,11 +37,11 @@ llista de números 'list_numbers', s'han de considerar els casos en què les
 dades que es troben dins de la llista siguin de tipus string o que la llista
 estigui buida.
 
-Addicionalment, la funció ha d'anar imprimint quin és el name menor i el
-name més gran segons va avançant a la llista sempre que aquest sigui diferent
+Addicionalment, la funció ha d'anar imprimint quin és el nombre menor i el
+nombre més gran segons va avançant a la llista sempre que aquest sigui diferent
 a l'anterior resultat simulant la depuració per traces.
 
-I finalment, retornar un valor del name més petit i del name més gran.
+I finalment, retornar un valor del nombre més petit i del nombre més gran.
 
 Paràmetres:
 list_numbers: Llista de números.
@@ -67,8 +67,33 @@ Exemple:
 
 
 def calculate_max_and_min(list_numbers):
-    # Write here your code
-    pass
+    # Comprovar si la llista es buida
+    if not list_numbers:
+        raise ValueError("Error: La llista esta buida")
+
+    # Comprovar el tipus de list_numbers
+    for num in list_numbers:
+        if not isinstance(num, (int, float)):
+            raise TypeError("El valor ha de ser de tipus int o float")
+    
+    # Inicalització de min y max
+    min_value = max_value = list_numbers[0]
+    print("Greater:", max_value)
+    print("Lesser:", min_value)
+
+    # Comencem al segon element
+    for num in list_numbers[1:]:
+        if num > max_value:
+            max_value = num
+            print(f"Greater: {max_value}")
+        
+        if num < min_value:
+            min_value = num
+            print(f"Lesser: {min_value}") 
+    
+    return min_value, max_value
+
+
 
 
 # Si quieres probar tu código, descomenta las siguientes líneas y ejecuta el script
